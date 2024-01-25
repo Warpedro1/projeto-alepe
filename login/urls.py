b729e792c1e13df45fccf66fbc72ff119e04b84e
Pedro Oliveira Pessoa Guerra <ppguerra@alepe.pe.gov.br>
2024-01-25 09:43:58 -0300

    Autenticacao de login Bem Sucedida

--- login/urls.py
+++ login/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from login.views import index
+from login.views import index, login_concluido
 
 urlpatterns = [
     path('', index),
+    path('login_concluido', login_concluido, name='login_concluido')
 ]

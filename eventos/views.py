6e75e15cfd77bb579a5b7012e5402cefffe48976
ALEPE//afbarbosa <afbarbosa@alepe.pe.gov.br>
2024-02-26 14:39:43 -0300

    função para mostrar os eventos de amanha adicionada.

--- eventos/views.py
+++ eventos/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from eventos.models import Evento
+from datetime import datetime, timedelta
 
 def home_eventos(request):
     # contagem de eventos por status
@@ -8,18 +9,24 @@ def home_eventos(request):
     # pegar usuario atual
     current_user = request.user
 
+    # pegar todos os eventos
+    eventos = get_all_eventos()
+
+    # pegar eventos amanha
+    eventos_amanha = get_eventos_amanha()
 
 
 
     return render(request, 'eventos/home_eventos.html', {
-        #'evento' : evento,
+        'eventos_amanha' : eventos_amanha,
+        'eventos' : eventos,
         'current_user' : current_user,
         'quantidade_eventos' : quantidade_eventos,
         })
 
 
 def get_all_status():
-    eventos = Evento.objects.all()
+    eventos = get_all_eventos()
     quantidade_eventos = {
         "agendados" : eventos.filter(status='AGENDADO').count(),
         "em_progresso" : eventos.filter(status='EM PROGRESSO').count(),
@@ -28,9 +35,28 @@ def get_all_status():
     }
     return quantidade_eventos
 
+def get_all_eventos():
+    # retorna uma query
+    return Evento.objects.all()
+
+def get_eventos_amanha():
+    # Obtenha a data de amanhã
+    data_amanha = (datetime.now() + timedelta(days=1)).date()
+
+    # Filtrar os eventos de amanhã
+    eventos_amanha = Evento.objects.filter(tempo__date=data_amanha)
+    return eventos_amanha
 
 """
 	colocar o card do evento dentro de um for
     botao de logout funcional
 """
 
+"""
+sample = Evento.objects.all()
+sa = sample.first()
+hoje = datetime.date.today()
+print(type(hoje))
+print(type(sa.tempo))
+
+"""

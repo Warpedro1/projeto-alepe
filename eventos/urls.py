617b08fa2be57b4942813a11a5690279fb1987fe
arthurbarbosaa <arthur.com50@gmail.com>
2024-03-04 15:16:36 -0300

    datas dos eventos de amanha a eventos do mes atual responsivas.

--- eventos/urls.py
+++ eventos/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from eventos.views import home_eventos
+from eventos.views import home_eventos, logout_view
+#from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
-    path('home_eventos', home_eventos, name='home_eventos')
+    path('home_eventos', home_eventos, name='home_eventos'),
+    path('', logout_view)
+    #path('logout/', LogoutView.as_view(), name='logout'),
 ]
--- eventos/views.py
+++ eventos/views.py
@@ -1,7 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth.decorators import login_required
 from eventos.models import Evento
 from datetime import datetime, timedelta
+from django.contrib.auth import logout
+from login.views import index
 
+
+
+def logout_view(request):
+    logout(request)
+    
+    return render(request, 'eventos/home_eventos.html')
+
+#@login_required(login_url='index.html')
 def home_eventos(request):
     # contagem de eventos por status
     quantidade_eventos = get_all_status()
@@ -15,13 +26,25 @@ def home_eventos(request):
     # pegar eventos amanha
     eventos_amanha = get_eventos_amanha()
 
+    # quantidade de eventos neste mes
+    quantidade_eventos_mes_atual = get_eventos_mes_atual().count()
+
+    datas = {
+        'hoje' : datetime.now().date().strftime("%d/%m"),
+        'amanha' : (datetime.now() + timedelta(days=1)).date().strftime("%d/%m"),
+    }
+
+    
+
 
 
     return render(request, 'eventos/home_eventos.html', {
+        'datas' : datas,
         'eventos_amanha' : eventos_amanha,
         'eventos' : eventos,
         'current_user' : current_user,
         'quantidade_eventos' : quantidade_eventos,
+        'quantidade_eventos_mes_atual' : quantidade_eventos_mes_atual,
         })
 
 
@@ -46,6 +69,15 @@ def get_eventos_amanha():
     # Filtrar os eventos de amanhã
     eventos_amanha = Evento.objects.filter(tempo__date=data_amanha)
     return eventos_amanha
+
+def get_eventos_mes_atual():
+    # Obtém o mês atual
+    mes_atual = datetime.now().month
+
+    # Filtra os eventos que ocorrem no mês atual
+    eventos_mes_atual = Evento.objects.filter(tempo__month=mes_atual)
+    return eventos_mes_atual
+
 
 """
 	colocar o card do evento dentro de um for

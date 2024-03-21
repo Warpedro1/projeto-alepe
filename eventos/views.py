ae62e5181511fc5deee4ef1b98c3fd098d652e6d
arthurbarbosaa <arthur.com50@gmail.com>
2024-03-21 15:00:10 -0300

    cards responsivos e status do dia atual.

--- eventos/views.py
+++ eventos/views.py
@@ -15,13 +15,20 @@ def logout_view(request):
 #@login_required(login_url='index.html')
 def home_eventos(request):
     # contagem de eventos por status
-    quantidade_eventos = get_all_status()
-
+    quantidade_eventos = {
+        'hoje': get_all_status_hoje(),
+        'total': get_all_status_total()
+    }
+    #get_all_status()
     # pegar usuario atual
     current_user = request.user
 
     # pegar todos os eventos
     eventos = get_all_eventos()
+
+    eventos_hoje = get_eventos_hoje()
+    for evento in eventos_hoje:
+        evento.tempo = evento.tempo.strftime("%H:%M")
 
     # pegar eventos amanha
     eventos_amanha = get_eventos_amanha()
@@ -29,17 +36,24 @@ def home_eventos(request):
     # quantidade de eventos neste mes
     quantidade_eventos_mes_atual = get_eventos_mes_atual().count()
 
-    datas = {
+    string_dias_da_semana = ["Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sabado", "Domingo"]
+
+    datas_em_string = {
+        'dia_da_semana': string_dias_da_semana[datetime.now().date().weekday()],
+        'hoje': datetime.now().date(),
+        'amanha': string_dias_da_semana[(datetime.now() + timedelta(days=1)).date().weekday()]
+    }
+
+    datas_formatadas = {
         'hoje' : datetime.now().date().strftime("%d/%m"),
         'amanha' : (datetime.now() + timedelta(days=1)).date().strftime("%d/%m"),
     }
 
-    
-
-
 
     return render(request, 'eventos/home_eventos.html', {
-        'datas' : datas,
+        'datas_em_string' : datas_em_string,
+        'datas_formatadas' : datas_formatadas,
+        'eventos_hoje': eventos_hoje,
         'eventos_amanha' : eventos_amanha,
         'eventos' : eventos,
         'current_user' : current_user,
@@ -48,8 +62,18 @@ def home_eventos(request):
         })
 
 
-def get_all_status():
+def get_all_status_total():
     eventos = get_all_eventos()
+    quantidade_eventos = {
+        "agendados" : eventos.filter(status='AGENDADO').count(),
+        "em_progresso" : eventos.filter(status='EM PROGRESSO').count(),
+        "concluidos" : eventos.filter(status='CONCLUIDO').count()
+
+    }
+    return quantidade_eventos
+
+def get_all_status_hoje():
+    eventos = get_eventos_hoje()
     quantidade_eventos = {
         "agendados" : eventos.filter(status='AGENDADO').count(),
         "em_progresso" : eventos.filter(status='EM PROGRESSO').count(),
@@ -61,6 +85,12 @@ def get_all_status():
 def get_all_eventos():
     # retorna uma query
     return Evento.objects.all()
+
+def get_eventos_hoje():
+    data_hoje = datetime.now().date()
+    eventos_hoje = Evento.objects.filter(tempo__date=data_hoje).order_by('tempo')
+
+    return eventos_hoje #querry
 
 def get_eventos_amanha():
     # Obtenha a data de amanhã
